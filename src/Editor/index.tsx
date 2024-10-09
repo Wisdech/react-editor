7170084385e19cbe849f16f0d61d306c71791b0f
徐添翼 XuTianyi（xXutianyi） <xutianyi12@outlook.com>
2024-10-10 00:26:03 +0800

    Update Locales

--- src/Editor/index.tsx
+++ src/Editor/index.tsx
@@ -1,12 +1,17 @@
 import EditorJS, { EditorConfig, OutputData } from '@editorjs/editorjs';
+import Header from '@editorjs/header';
 import ImageTool from '@editorjs/image';
 import LinkTool from '@editorjs/link';
-import { defaultData, toolsConfig } from './config';
-
+import List from '@editorjs/list';
+import Quote from '@editorjs/quote';
+import Table from '@editorjs/table';
+import Underline from '@editorjs/underline';
+import Warning from '@editorjs/Warning';
 import clsx from 'clsx';
 import React, { useEffect, useRef, useState } from 'react';
 
 import styles from '../editor.module.css';
+import i18n from './locales/zh.json';
 
 export type EditorProps = {
   setInstance?: (instance: EditorJS | null) => void;
@@ -25,20 +30,39 @@ export function Editor({ data, endpoints, setInstance }: EditorProps) {
 
   const defaultConfig: EditorConfig = {
     placeholder: '输入或添加内容...',
-    data: data ?? defaultData,
+    data: data ?? {
+      blocks: [
+        { type: 'header', data: { text: '', level: 1 } },
+        { type: 'paragraph', data: { text: '' } },
+      ],
+    },
+    i18n: { messages: i18n },
     tools: {
-      ...toolsConfig,
-
-      linkTool: {
-        class: LinkTool,
-        config: { endpoint: endpoints?.linkUrl },
+      header: {
+        class: Header,
+        config: { levels: [1, 2, 3, 4], placeholder: '输入标题' },
       },
-
+      list: List,
       image: {
         class: ImageTool,
         config: {
           endpoints: { byFile: endpoints?.imageFile, byUrl: endpoints?.imageUrl },
         },
+      },
+      table: Table,
+      warning: {
+        class: Warning,
+        config: { titlePlaceholder: '提示标题', messagePlaceholder: '提示内容' },
+      },
+      quote: Quote,
+      linkTool: {
+        class: LinkTool,
+        config: { endpoint: endpoints?.linkUrl },
+      },
+
+      underline: {
+        class: Underline,
+        shortcut: 'CMD+U',
       },
     },
   };
